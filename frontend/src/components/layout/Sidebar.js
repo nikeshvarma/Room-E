116889cecf66257ffe0f63bdd13d19bf5e9eec7a
Nikesh Varma <nikeshvarma50@gmail.com>
2021-02-21 21:50:40 +0530

    Sidebar Created

--- frontend/src/components/layout/Sidebar.js
+++ frontend/src/components/layout/Sidebar.js
@@ -6,23 +6,28 @@ const Sidebar = () => {
     return (
         <div className="side-tab text-center">
             <ListGroup>
+                <NavLink to="/">
+                    <ListGroup.Item className="nav-link-style">
+                        Home
+                    </ListGroup.Item>
+                </NavLink>
                 <NavLink to="/signup">
-                    <ListGroup.Item>
+                    <ListGroup.Item className="nav-link-style">
                         Sign Up
                     </ListGroup.Item>
                 </NavLink>
                 <NavLink to="/login">
-                    <ListGroup.Item>
+                    <ListGroup.Item className="nav-link-style">
                         Login
                     </ListGroup.Item>
                 </NavLink>
                 <NavLink to="/about">
-                    <ListGroup.Item>
+                    <ListGroup.Item className="nav-link-style">
                         About
                     </ListGroup.Item>
                 </NavLink>
                 <NavLink to="/contact">
-                    <ListGroup.Item>
+                    <ListGroup.Item className="nav-link-style">
                         Contact
                     </ListGroup.Item>
                 </NavLink>

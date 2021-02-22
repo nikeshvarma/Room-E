5461ef07bc29e3c585c981bb14a6b40da8597929
Nikesh Varma <nikeshvarma50@gmail.com>
2021-02-22 23:19:51 +0530

    Redux Store Created

--- frontend/src/components/pages/Login.js
+++ frontend/src/components/pages/Login.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import "../../style/auth.css";
+import {TextField} from "@material-ui/core";
+import Button from "@material-ui/core/Button";
+import {Https} from "@material-ui/icons";
+import {Card} from "react-bootstrap";
+
 
 const Login = () => {
     return (
         <div>
-            <h1>Login</h1>
+            <div className="d-flex justify-content-center login-form">
+                <Card className="m-5">
+                    <div className="card-body shadow">
+                        <div className="d-flex justify-content-center">
+                            <h3>LOGIN</h3>
+                        </div>
+                        <form>
+                            <TextField fullWidth margin="normal" id="outlined-basic" label="Email" variant="outlined"/>
+                            <br/>
+                            <TextField fullWidth margin="normal" id="outlined-basic" label="Password" variant="outlined"/>
+                            <Button
+                                className="text-center"
+                                variant="contained"
+                                color="primary"
+                                endIcon={<Https/>}
+                            >
+                                Login
+                            </Button>
+                        </form>
+                    </div>
+                </Card>
+            </div>
         </div>
     );
 };

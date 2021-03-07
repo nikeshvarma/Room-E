c03796515265b80c3c186e8530650ba879e6d6ce
Shailendra <shailendrayadav25072001@gmail.com>
2021-03-07 23:55:49 +0530

    home model created and regisrted

--- backend/HOME/admin.py
+++ backend/HOME/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import House
+
+
+@admin.register(House)
+class AdminHouse(admin.ModelAdmin):
+    pass
--- backend/HOME/models.py
+++ backend/HOME/models.py
@@ -1,1 +1,19 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+
+User = get_user_model()
+
+
+class House(models.Model):
+    # HOUSE_TYPE=[
+    #   ()
+    # ]
+    home_owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    home_price = models.FloatField()
+    home_address = models.TextField()
+
+    # image=models.ImageField()
+    # home_type = models.CharField(max_length=4)
+    class Meta:
+        def __str__(self):
+            return self.home_owner

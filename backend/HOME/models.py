ba1308f58b8f3e605ad1543dac3692b71cfc8847
Nikesh Varma <nikeshvarma50@gmail.com>
2021-03-08 00:08:51 +0530

    ADDED USERS

--- backend/HOME/models.py
+++ backend/HOME/models.py
@@ -5,15 +5,9 @@ User = get_user_model()
 
 
 class House(models.Model):
-    # HOUSE_TYPE=[
-    #   ()
-    # ]
     home_owner = models.ForeignKey(User, on_delete=models.CASCADE)
     home_price = models.FloatField()
     home_address = models.TextField()
 
-    # image=models.ImageField()
-    # home_type = models.CharField(max_length=4)
-    class Meta:
-        def __str__(self):
-            return self.home_owner
+    def __str__(self):
+        return str(self.home_owner)

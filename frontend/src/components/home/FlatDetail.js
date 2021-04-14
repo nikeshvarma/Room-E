18ccb2627cb13423ff8c9faf50a53cfbf12438aa
Nikesh Varma <nikeshvarma50@gmail.com>
2021-04-14 23:15:08 +0530

    fontawesome added

--- frontend/src/components/home/FlatDetail.js
+++ frontend/src/components/home/FlatDetail.js
@@ -1,11 +1,12 @@
 import React, {useEffect, useState} from 'react';
 import axios from "axios";
 import Loading from "../layout/Loading";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {Carousel, Col, Row} from "react-bootstrap";
 
 const FlatDetail = (props) => {
 
-    const [loading, setLoading] = useState(false);
+    const [loading, setLoading] = useState(true);
     const [property, setProperty] = useState([]);
     const propertyId = props.match?.params?.flatId;
 
@@ -24,7 +25,7 @@ const FlatDetail = (props) => {
     let flatDisplay = property.map((flat, index) =>
         <div key={index}>
             <Row>
-                <Col md={6}>
+                <Col md={8}>
                     <div className="img-responsive img-box">
                         <Carousel>
                             {flat.images.map((image, index) =>
@@ -40,8 +41,43 @@ const FlatDetail = (props) => {
                     </div>
                 </Col>
 
-                <Col md={6}>
-
+                <Col md={4}>
+                    <table className="text-center w-100 table-bordered" cellPadding="5px">
+                        <tbody>
+                        <tr>
+                            <td>
+                                <h6 className="text-secondary mt-2"><FontAwesomeIcon icon="hand-holding-usd" color="orange"/> Rent</h6>
+                                <p className="mb-0">{flat.rent}/month</p>
+                            </td>
+                            <td>
+                                <h6 className="text-secondary mt-2"><FontAwesomeIcon icon="user-shield" color="green"/> Security Deposite</h6>
+                                <p className="mb-0">&#x20B9; {flat.security_deposit}</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <h6 className="text-secondary mt-2"><FontAwesomeIcon icon="users" color="#87CEEB"/> Available For</h6>
+                                <p className="mb-0">{flat.available_for}</p>
+                            </td>
+                            <td>
+                                <h6 className="text-secondary mt-2"><FontAwesomeIcon icon="home" color="#8000FF"/> Flat Type</h6>
+                                <p className="mb-0">{flat.rooms} BHK</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colSpan="2">
+                                <h6 className="text-secondary mt-2"><FontAwesomeIcon icon="map-marked-alt" color="#617DA6"/> Address</h6>
+                                <p className="text-left px-2 mb-0">{flat.address}</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colSpan="2">
+                                <h6 className="text-secondary mt-2"><FontAwesomeIcon icon="file-contract" color="#FFE000"/> Posted On</h6>
+                                <p className="text-left px-2 mb-0">{flat.post_datetime}</p>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
                 </Col>
             </Row>
         </div>
